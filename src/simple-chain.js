425957d8c469f2d82cb31ca3ec30f77e30b3846b
Ria River <riria.river@gmail.com>
2023-12-25 15:22:41 +0300

    feat: implement 'Chain maker' task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,49 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.chain.push(`( ${(value)} )`);
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    const ind = position - 1;
+
+    if (this.chain[ind] === undefined) {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+
+    this.chain.splice(ind, 1);
+
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const chainStr = this.chain.join('~~');
+
+    this.chain = [];
+
+    return chainStr;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
